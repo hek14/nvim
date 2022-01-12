7bd93ba06bfd9e312ec4c36ac2630fa0411a6e57
hek14 <1023129548@qq.com>
2022-01-12 19:04:08 +0800

    Fixup: gps components in statusline

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -250,7 +250,7 @@ customPlugins.add(function(use)
           ["class-name"] = ' ',      -- Classes and class-like objects
           ["function-name"] = ' ',   -- Functions
           ["method-name"] = ' ',     -- Methods (functions inside class-like objects)
-          ["container-name"] = '⛶ ',  -- Containers (example: lua tables)
+          ["container-name"] = ' ',  -- Containers (example: lua tables)
           ["tag-name"] = '炙'         -- Tags (example: html tags)
         },
       })

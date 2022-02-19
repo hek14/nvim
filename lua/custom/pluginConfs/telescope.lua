4f3d0af8b64056ed2779c4965b7169df2d859895
hek14 <1023129548@qq.com>
2022-02-19 11:08:02 +0800

    Fix Telescope extension loading: setup() function is not called

--- lua/custom/pluginConfs/telescope.lua
+++ lua/custom/pluginConfs/telescope.lua
@@ -1,110 +1,98 @@
 local present, telescope = pcall(require, "telescope")
 
-if not present then
-   return
-end
+if not present then return end
 
 local fixfolds = {
-  hidden = true,
-  attach_mappings = function(_)
-    require("telescope.actions.set").select:enhance({
-      post = function()
-        vim.cmd[[normal! zx]]
-      end,
-    })
-    return true
-  end,
+    hidden = true,
+    attach_mappings = function(_)
+        require("telescope.actions.set").select:enhance({
+            post = function() vim.cmd [[normal! zx]] end
+        })
+        return true
+    end
 }
 
 local default = {
-   pickers = {
-      buffers = fixfolds,
-      file_browser = fixfolds,
-      find_files = fixfolds,
-      git_files = fixfolds,
-      grep_string = fixfolds,
-      live_grep = fixfolds,
-      oldfiles = fixfolds,
-      lsp_definitions = fixfolds,
-      lsp_references = fixfolds,
-      lsp_document_symbols = fixfolds,
-      lsp_workspace_symbols = fixfolds,
-      lsp_dynamic_workspace_symbols = fixfolds,
+    pickers = {
+        buffers = fixfolds,
+        file_browser = fixfolds,
+        find_files = fixfolds,
+        git_files = fixfolds,
+        grep_string = fixfolds,
+        live_grep = fixfolds,
+        oldfiles = fixfolds,
+        lsp_definitions = fixfolds,
+        lsp_references = fixfolds,
+        lsp_document_symbols = fixfolds,
+        lsp_workspace_symbols = fixfolds,
+        lsp_dynamic_workspace_symbols = fixfolds
     },
-   defaults = {
-      vimgrep_arguments = {
-         "rg",
-         "--color=never",
-         "--no-heading",
-         "--with-filename",
-         "--line-number",
-         "--column",
-         "--smart-case",
-      },
-    -- mappings = {
-    --   i = {
-    --     ["<cr>"] = function(prompt_bufnr)
-    --       require('telescope.actions').select_default(prompt_bufnr)
-    --       -- vim.cmd[[normal! zv]]
-    --       vim.cmd[[echom "telescope hello"]]
-    --     end}
-    -- },
-      prompt_prefix = "   ",
-      selection_caret = "  ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "ascending",
-      layout_strategy = "horizontal",
-      layout_config = {
-         horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8,
-         },
-         vertical = {
-            mirror = false,
-         },
-         width = 0.87,
-         height = 0.80,
-         preview_cutoff = 120,
-      },
-      file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = { "node_modules" },
-      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-      path_display = { "truncate" },
-      winblend = 0,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      color_devicons = true,
-      use_less = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-   },
-   extensions = {
-     bookmarks = {
-       -- Available: 'brave', 'chrome', 'edge', 'firefox', 'safari'
-       selected_browser = 'chrome',
-     },
-  }
+    defaults = {
+        vimgrep_arguments = {
+            "rg", "--color=never", "--no-heading", "--with-filename",
+            "--line-number", "--column", "--smart-case"
+        },
+        -- mappings = {
+        --   i = {
+        --     ["<cr>"] = function(prompt_bufnr)
+        --       require('telescope.actions').select_default(prompt_bufnr)
+        --       -- vim.cmd[[normal! zv]]
+        --       vim.cmd[[echom "telescope hello"]]
+        --     end}
+        -- },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8
+            },
+            vertical = {mirror = false},
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = {"node_modules"},
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = {"truncate"},
+        winblend = 0,
+        border = {},
+        borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+        color_devicons = true,
+        use_less = true,
+        set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
+    },
+    extensions = {
+        bookmarks = {
+            -- Available: 'brave', 'chrome', 'edge', 'firefox', 'safari'
+            selected_browser = 'chrome'
+        }
+    }
 }
 
 local M = {}
-M.setup = function(override_flag)
-   if override_flag then
-      default = require("core.utils").tbl_override_req("telescope", default)
-   end
-
-   telescope.setup(default)
-
-  local extensions = { "themes", "terms", "bookmarks", "neoclip", "projects", "zoxide", "file_browser"}
-  for _, ext in ipairs(extensions) do
-    telescope.load_extension(ext)
-  end
+if override_flag then
+    default = require("core.utils").tbl_override_req("telescope", default)
 end
+
+telescope.setup(default)
+
+local extensions = {
+    "themes", "terms", "bookmarks", "neoclip", "projects", "zoxide",
+    "file_browser"
+}
+for _, ext in ipairs(extensions) do telescope.load_extension(ext) end
 
 return M

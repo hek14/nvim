8017dc02bf6e1f2d1787ee7c23abffd91615f6fd
hek14 <1023129548@qq.com>
2022-02-28 17:56:51 +0800

    (fix)dap: focus on repl when opening dap

--- lua/custom/pluginConfs/dap.lua
+++ lua/custom/pluginConfs/dap.lua
@@ -26,7 +26,7 @@ map('n', '<F4>', ':lua require"dap".run_to_cursor()<CR>')
 map('n', '<F8>', ':lua require"dap".toggle_breakpoint()<CR>')
 map('n', '<leader>dn', ':lua require"dap".down()<CR>')
 map('n', '<leader>de', ':lua require"dap".up()<CR>')
-map('n', '<leader>dq', ':lua require"dap".close()<CR>')
+map('n', '<leader>dq', ':lua require"dap".close()<CR>:lua require("dapui").close()<CR>')
 map('n', '<leader>dr', ':lua require"dap".repl.toggle({},"10 split")<CR>')
 map('n', '<leader>di', ':lua require"dap.ui.widgets".hover()<CR>')
 map('n', '<leader>d?', ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>')
@@ -36,11 +36,12 @@ require('telescope').load_extension('dap')
 map('n', '<leader>ds', ':Telescope dap frames<CR>')
 map('n', '<leader>dc', ':Telescope dap commands<CR>')
 map('n', '<leader>db', ':Telescope dap list_breakpoints<CR>')
-vim.cmd [[  au FileType dap-repl lua require('dap.ext.autocompl').attach() ]]
+vim.cmd [[ autocmd FileType dap-repl lua require('dap.ext.autocompl').attach() ]]
+vim.cmd [[ autocmd BufWinEnter,BufEnter,WinEnter * if &filetype=="dap-repl" | startinsert | endif ]]
 
 -- theHamsta/nvim-dap-virtual-text and mfussenegger/nvim-dap
 require('nvim-dap-virtual-text').setup({
-  enabled = false,                     -- enable this plugin (the default)
+  enabled = true,                     -- enable this plugin (the default)
   enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
   highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
   highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
@@ -53,14 +54,48 @@ require('nvim-dap-virtual-text').setup({
   virt_text_win_col = nil
 }
 )
+require("dapui").setup({
+  mappings = {
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "<C-e>",
+    repl = "r",
+  }})
 local dapui = require("dapui")
-dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
+  local wins = vim.api.nvim_list_wins()
+  local target = nil
+  local curr_win = vim.api.nvim_get_current_win()
+  for _,win in ipairs(wins) do
+    vim.api.nvim_win_call(win,function ()
+      if "dap-repl"==vim.api.nvim_buf_get_option(0,"ft") then
+        target = win
+        vim.g.dap_repl_buffer = vim.api.nvim_win_get_buf(win)
+      end
+      vim.api.nvim_buf_set_keymap(0,"i","<C-w>e","<Esc><C-w>k<CR>",{noremap=true,silent=true})
+      vim.api.nvim_buf_set_keymap(0,"i","<C-w>n","<Esc><C-w>j<CR>",{noremap=true,silent=true})
+      vim.api.nvim_buf_set_keymap(0,"i","<C-w>i","<Esc><C-w>l<CR>",{noremap=true,silent=true})
+      vim.api.nvim_buf_set_keymap(0,"i","<C-w>h","<Esc><C-w>h<CR>",{noremap=true,silent=true})
+    end)
+  end
+
+  if target then
+    vim.api.nvim_set_current_win(target)
+  else
+    print("no dap-repl win opened, check nvim-dap docs")
+  end
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
   dapui.close()
+  -- vim.defer_fn(function() 
+  --   vim.api.nvim_buf_delete(vim.g.dap_repl_buffer,{force=true})
+  -- end,0)
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
+  -- vim.defer_fn(function() 
+  --   vim.api.nvim_buf_delete(vim.g.dap_repl_buffer,{force=true})
+  -- end,0)
 end

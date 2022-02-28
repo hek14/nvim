e31407b38c819697859dfd67279e83503bd175fd
hek14 <1023129548@qq.com>
2022-02-28 14:32:09 +0800

    Add my personal pattern of using project.lua ".project"

--- lua/custom/pluginConfs/project.lua
+++ lua/custom/pluginConfs/project.lua
@@ -16,7 +16,7 @@ M.setup = function ()
 
     -- All the patterns used to detect root dir, when **"pattern"** is in
     -- detection_methods
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.py", "trainer*.py"},
+    patterns = { ".git", ".project", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.py", "trainer*.py"},
 
     -- Table of lsp clients to ignore by name
     -- eg: { "efm", ... }

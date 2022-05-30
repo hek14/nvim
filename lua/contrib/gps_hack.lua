4a6a2ee2d1b7f286707eb8da43b2ab0514d24435
hek14 <1023129548@qq.com>
2022-05-31 04:08:40 +0800

    Fix: gps_hack: jump to parent if current_line is at the begin of current_node

--- lua/contrib/gps_hack.lua
+++ lua/contrib/gps_hack.lua
@@ -14,15 +14,21 @@ local transform = function(text)
   end
 end
 
+local update_tree = ts_utils.memoize_by_buf_tick(function(bufnr)
+  local filelang = ts_parsers.ft_to_lang(vim.api.nvim_buf_get_option(bufnr, "filetype"))
+  local parser = ts_parsers.get_parser(bufnr, filelang)
+  return parser:parse()
+end)
 
 M.gps_context_parent = function(level_up)
   local level_up = level_up and level_up or vim.v.count1
   local filelang = ts_parsers.ft_to_lang(vim.bo.filetype)
   local gps_query = ts_queries.get_query(filelang, "nvimGPS")
-  local data = {}
   local node = ts_utils.get_node_at_cursor()
+  update_tree(vim.api.nvim_get_current_buf())
 
-  local function jump_to_parent_node(pos,capture_name,capture_node)
+  local data = {}
+  local function add_one_node(pos,capture_name,capture_node)
     local text = ""
     text = vim.treesitter.query.get_node_text(capture_node, 0)
     text = string.gsub(text, "%s+", ' ')
@@ -31,45 +37,67 @@ M.gps_context_parent = function(level_up)
     table.insert(data,pos,{
       type = transform(capture_name),
       text = text,
-      range = {start_row+1,start_col}
+      range = {['start']={start_row+1,start_col},['end']={end_row,end_col}}
     })
   end
 
+  local cnt = 0
   while node do
     local iter = gps_query:iter_captures(node, 0)
     local capture_ID, capture_node = iter()
 
     if capture_node == node then
+      cnt = cnt + 1
       if gps_query.captures[capture_ID] == "scope-root" then
 
         while capture_node == node do
           capture_ID, capture_node = iter()
         end
         local capture_name = gps_query.captures[capture_ID]
-        jump_to_parent_node(1,capture_name,capture_node)
+        add_one_node(1,capture_name,capture_node)
 
       elseif gps_query.captures[capture_ID] == "scope-root-2" then
 
         capture_ID, capture_node = iter()
         local capture_name = gps_query.captures[capture_ID]
-        jump_to_parent_node(1,capture_name,capture_node)
+        add_one_node(1,capture_name,capture_node)
 
         capture_ID, capture_node = iter()
         capture_name = gps_query.captures[capture_ID]
-        jump_to_parent_node(2,capture_name,capture_node)
+        add_one_node(2,capture_name,capture_node)
       end
     end
     node = node:parent()
   end
 
-  local target = data[#data-level_up+1]
+  local merged_data = {}
+  for _, v in pairs(data) do
+    if not vim.tbl_islist(v) then
+      table.insert(merged_data, v)
+    else
+      vim.list_extend(merged_data, v)
+    end
+  end
+
+  local index = #merged_data-level_up+1
+  local target = merged_data[index]
+  local curr = vim.api.nvim_win_get_cursor(0)
+  while target.range['start'][1]==curr[1] do
+    index = index-1
+    target = merged_data[index]
+    if index <= 0 then
+      break
+    end
+  end
+
   if target == nil then
     print("no parent target")
     return
   end
+
   vim.cmd("normal! m'")
   print(string.format("jump to %s: %s", target.type, target.text))
-  vim.api.nvim_win_set_cursor(0,target.range)
+  vim.api.nvim_win_set_cursor(0,target.range['start'])
   vim.cmd("normal! zv")
 end
 

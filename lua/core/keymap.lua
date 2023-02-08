9351698fd5579ade414f7ede215e344ffb25c1d3
hek14 <1023129548@qq.com>
2023-02-08 17:46:38 +0800

    fix

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -155,27 +155,19 @@ local function others()
   return "'" . substitute(copy(a:str), "'", "''", 'g') . "'"
   endfunction
 
-  call Cabbrev('pi', 'PackerInstall')
-  call Cabbrev('pud', 'PackerUpdate')
-  call Cabbrev('pc', 'PackerCompile')
-  call Cabbrev('ps', 'PackerSync')
+  call Cabbrev('LI', 'Lazy install')
+  call Cabbrev('LU', 'Lazy update')
+  call Cabbrev('LS', 'Lazy sync')
+  call Cabbrev('LP', 'Lazy profile')
+  call Cabbrev('LC', 'Lazy clean')
   call Cabbrev('li', 'let i =1 \|')
-  call Cabbrev('py', 'PYTHON')
   call Cabbrev('lg', 'Lazygit')
-  call Cabbrev('ft', 'FloatermNew')
   xnoremap @ :<C-u>call ExecuteMacroOverVisualRange()<CR>
   function! ExecuteMacroOverVisualRange()
   echo "@".getcmdline()
   execute ":'<,'>normal @".nr2char(getchar())
   endfunction
   ]])
-  -- Add Packer commands because we are not loading it at startup
-  cmd "silent! command! PackerClean lua require 'plugins' require('packer').clean()"
-  cmd "silent! command! PackerCompile lua require 'plugins' require('packer').compile()"
-  cmd "silent! command! PackerInstall lua require 'plugins' require('packer').install()"
-  cmd "silent! command! PackerStatus lua require 'plugins' require('packer').status()"
-  cmd "silent! command! PackerSync lua require 'plugins' require('packer').sync()"
-  cmd "silent! command! PackerUpdate lua require 'plugins' require('packer').update()"
 end
 
 others()

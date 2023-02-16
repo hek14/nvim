221db78371f2f623229c08d9bdbb31baea5b8837
hek14 <1023129548@qq.com>
2023-02-17 01:57:16 +0800

    add reference_hint for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,11 @@
 local M = {
   "nvim-lualine/lualine.nvim",
-  enabled = false,
-  dependencies = { "hek14/nvim-navic","hek14/vim-illuminate","nvim-tree/nvim-web-devicons" },
+  dependencies = { 
+    "hek14/nvim-navic",
+    "hek14/vim-illuminate",
+    "nvim-tree/nvim-web-devicons",
+    "yorik1984/newpaper.nvim"
+  },
   event = "VeryLazy",
 }
 
@@ -27,47 +31,46 @@ local inactive_winbar = function ()
   return filename
 end
 
+local reference_hint = function ()
+  local buf = vim.api.nvim_get_current_buf()
+  local ok,illuminate = require('illuminate')
+  if not ok then
+    return ""
+  else
+    local refs = _G.illuminate_references[buf]
+    local current_loc = nil
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local current_col = vim.api.nvim_win_get_cursor(0)[2]
+    for i,ref in ipairs(refs) do
+      local _start = ref.range.start
+      local _end = ref.range['end']
+      local condition = _start.line+1 == current_line and
+      _start.character <= current_col and
+      _end.line+1 >= current_line and
+      _end.character >= current_col
+      if condition then
+        current_loc = i
+        break
+      end
+    end
+
+    return string.format("reference: %d|%d",current_loc,#refs)
+  end
+end
+
 vim.opt.laststatus = 3
 M.config = function ()
   require('lualine').setup {
     options = {
       globalstatus = true,
     },
-    winbar = {
-      lualine_a = { winbar },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {}
-    },
-    inactive_winbar = {
-      lualine_a = { inactive_winbar }
-    },
+    winbar = {},
+    inactive_winbar = {},
     sections = {
       lualine_a = {'mode'},
       lualine_b = {'branch', 'diff', 'diagnostics'},
       lualine_c = {},
-      lualine_x = {
-        {
-          'buffers',
-          show_modified_status = true, -- Shows indicator when the buffer is modified.
-          mode = 4,
-          -- 0: Shows buffer name
-          -- 1: Shows buffer index
-          -- 2: Shows buffer name + buffer index
-          -- 3: Shows buffer number
-          -- 4: Shows buffer name + buffer number
-          max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-          -- it can also be a function that returns
-          -- the value of `max_length` dynamically.
-          symbols = {
-            modified = ' ●',      -- Text to show when the buffer is modified
-            alternate_file = '#', -- Text to show to identify the alternate file
-            directory =  '',     -- Text to show when the buffer is a directory
-          },
-        }
-      },
+      lualine_x = {reference_hint},
       lualine_y = {'filetype'},
       lualine_z = {'progress'}
     },

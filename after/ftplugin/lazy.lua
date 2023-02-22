972ea65d314eaefbe2aa6d428722953d0dd23fe9
hek14 <1023129548@qq.com>
2023-02-23 00:10:56 +0800

    my lazy: use virtual text to hint status instead of winbar

--- after/ftplugin/lazy.lua
+++ after/ftplugin/lazy.lua
@@ -5,6 +5,8 @@ local timer2 = nil
 local not_loaded_line = nil
 local buf = vim.api.nvim_get_current_buf()
 local win = vim.api.nvim_get_current_win()
+local win_width = vim.api.nvim_win_get_width(win)
+local ns = vim.api.nvim_create_namespace("_lazy")
 
 local where_not_loaded = function()
   local cursor = vim.api.nvim_win_get_cursor(win)
@@ -27,7 +29,7 @@ vim.api.nvim_create_autocmd("TextChanged",{
   callback = function ()
     if not timer then
       timer = vim.loop.new_timer()
-      timer:start(100,0,vim.schedule_wrap(function ()
+      timer:start(10,0,vim.schedule_wrap(function ()
         pcall(where_not_loaded)
       end))
     end
@@ -53,6 +55,31 @@ local set_winbar = function ()
   end
 end
 
+local set_virtual_text = function ()
+  if not_loaded_line then
+    local cursor = vim.api.nvim_win_get_cursor(win)
+    local line = cursor[1]
+    if line < not_loaded_line then
+      vim.api.nvim_buf_set_extmark(buf,ns,cursor[1],cursor[2],{
+        id = 1,
+        virt_text = { { "Loaded","DiagnosticVirtualTextHint" } },
+        virt_text_win_col = math.floor(win_width*0.5)
+      })
+    else
+      vim.api.nvim_buf_set_extmark(buf,ns,cursor[1],cursor[2],{
+        id = 1,
+        virt_text = { { "Not Loaded","DiagnosticVirtualTextError" } },
+        virt_text_win_col = math.floor(win_width*0.5)
+      })
+    end
+  end
+  if timer2 then
+    timer2:stop()
+    timer2:close()
+    timer2 = nil
+  end
+end
+
 vim.api.nvim_create_autocmd(
 { "CursorMoved", "CursorHold" },
 {
@@ -60,12 +87,22 @@ vim.api.nvim_create_autocmd(
   buffer = buf,
   callback = function()
     timer2 = vim.loop.new_timer()
-    timer2:start(100,0,vim.schedule_wrap(function ()
-      pcall(set_winbar)
+    timer2:start(10,0,vim.schedule_wrap(function ()
+      pcall(set_virtual_text)
     end))
   end,
 }
 )
+
+vim.api.nvim_create_autocmd("WinClosed",
+{
+  group = group,
+  buffer = buf,
+  callback = function ()
+    print('Lazy closed')
+    vim.api.nvim_buf_clear_namespace(buf,ns,0,-1) 
+  end
+})
 
 local goto_plugin = function()
   local plugin = vim.fn.input('Goto plugin: ')

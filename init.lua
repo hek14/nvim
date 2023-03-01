7a2eb7867fe44894519cfef098dd05766270caf1
hek14 <1023129548@qq.com>
2023-03-01 09:37:28 +0800

    update

--- init.lua
+++ init.lua
@@ -1,22 +1,3 @@
--- deprecated: use :Lazy profile instead
--- vim.api.nvim_create_autocmd("UIEnter", {
---   callback = function()
---     local is_mac = vim.loop.os_uname().sysname=="Darwin"
---     if is_mac then
---       print('please use startuptime to profile')
---       return
---     else
---       local pid = vim.loop.os_getpid()
---       local ctime = vim.loop.fs_stat("/proc/" .. pid).ctime
---       local start = ctime.sec + ctime.nsec / 1e9
---       local tod = { vim.loop.gettimeofday() }
---       local now = tod[1] + tod[2] / 1e6
---       local startuptime = (now - start) * 1000
---       vim.notify("startup: " .. startuptime .. "ms")
---     end
---   end,
--- })
-
 local modules = {
    "core.utils",
    "core.options",
@@ -27,10 +8,6 @@ local modules = {
 }
 
 for _, module in ipairs(modules) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
+  require(module)
 end
-require("core.utils").ppid()
 _G.p = require('contrib.print_to_buf').liveprint
--- lua/contrib/deprecated.lua
+++ lua/contrib/deprecated.lua
@@ -1,4 +1,22 @@
 local lprint = require("core.utils").log
+-- deprecated: use :Lazy profile instead
+-- vim.api.nvim_create_autocmd("UIEnter", {
+--   callback = function()
+--     local is_mac = vim.loop.os_uname().sysname=="Darwin"
+--     if is_mac then
+--       print('please use startuptime to profile')
+--       return
+--     else
+--       local pid = vim.loop.os_getpid()
+--       local ctime = vim.loop.fs_stat("/proc/" .. pid).ctime
+--       local start = ctime.sec + ctime.nsec / 1e9
+--       local tod = { vim.loop.gettimeofday() }
+--       local now = tod[1] + tod[2] / 1e6
+--       local startuptime = (now - start) * 1000
+--       vim.notify("startup: " .. startuptime .. "ms")
+--     end
+--   end,
+-- })
 function Buf_attach()
   -- reset keymaps
   vim.defer_fn(function ()

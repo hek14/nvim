2a377bcbc8388dd95c8e0a69ddaff867f78503ee
hek14 <1023129548@qq.com>
2023-03-18 02:29:27 +0800

    fix saga map

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -3,8 +3,30 @@ return {
   event = "BufRead",
   config = function()
     require("lspsaga").setup({
-      symbol_in_winbar = { enable = true }
+      symbol_in_winbar = { enable = true },
+      finder = {
+        keys = {
+          jump_to = 'p',
+          edit = { 'o', '<CR>' },
+          vsplit = 's',
+          split = 'i',
+          tabe = 't',
+          tabnew = 'r',
+          quit = { 'q', '<ESC>' },
+          close_in_preview = '<C-q>'
+        },
+      },
     })
+    local au = require('core.autocmds').au
+    au('FileType', {
+      callback = function ()
+        vim.defer_fn(function ()
+          vim.keymap.set('n','e','k',{noremap=true,nowait=true,silent=true,buffer=true})
+        end,5)
+      end,
+      pattern = 'lspsagafinder'
+    })
+      
   end,
   dependencies = {
     {"nvim-tree/nvim-web-devicons"},

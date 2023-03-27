9d877fbb8aed3d559dbc9b3037a2b40ec33ebd43
hek14 <1023129548@qq.com>
2023-03-27 16:18:26 +0800

    fix utils.log: get the lineinfo outside of the schedule

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -511,35 +511,37 @@ M.clear_log = function ()
   vim.fn.system('echo > ' .. log_path)
 end
 
-M.log = vim.schedule_wrap(function(...)
+M.log = function (...)
   local start = vim.loop.hrtime()
   local log_path = vim.fn.expand("$HOME") .. "/.cache/nvim/kk_debug.log"
-  local arg = {...}
   local str = "שׁ "
   local lineinfo = ''
 
   local info = debug.getinfo(2, "Sl")
   lineinfo = info.short_src .. ":" .. info.currentline
-  str = str .. lineinfo
-
-  for i, v in ipairs(arg) do
-    if type(v) == "table" then
-      str = str .. " |" .. tostring(i) .. ": " .. vim.inspect(v) .. "\n"
-    else
-      str = str .. " |" .. tostring(i) .. ": " .. tostring(v)
+  local date = os.date()
+  str = str .. date .. ", " .. str .. lineinfo .. '\n'
+  local arg = {...}
+  vim.schedule(function()
+    for i, v in ipairs(arg) do
+      if type(v) == "table" then
+        str = str .. " |" .. tostring(i) .. ": " .. vim.inspect(v) .. "\n"
+      else
+        str = str .. " |" .. tostring(i) .. ": " .. tostring(v)
+      end
     end
-  end
-  if #str > 2 then
-    if log_path ~= nil and #log_path > 3 then
-      local f = io.open(log_path, "a+")
-      io.output(f)
-      io.write(str .. "=======\n")
-      io.close(f)
-    else
-      print(str .. "\n")
+    if #str > 2 then
+      if log_path ~= nil and #log_path > 3 then
+        local f = io.open(log_path, "a+")
+        io.output(f)
+        io.write(str .. "=======\n")
+        io.close(f)
+      else
+        print(str .. "\n")
+      end
     end
-  end
-end)
+  end)
+end 
 
 M.source_curr_file = function()
   if vim.bo.ft == "lua" then

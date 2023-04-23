a06ee8fdf02cf8d4d9a441c71e2f238ce4130ee0
hek14 <1023129548@qq.com>
2023-04-23 21:37:07 +0800

    fix nvimtree config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,3 +1,15 @@
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
+  vim.keymap.set('n', 'N', api.node.navigate.sibling.last, opts('Last Sibling'))
+  vim.keymap.set('n', 'E', api.node.navigate.sibling.first, opts('First Sibling'))
+  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
+end
 local M = {
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeClose", "NvimTreeOpen"},
@@ -7,61 +19,10 @@ local M = {
   },
   config = function()
     require("nvim-tree").setup({
+      on_attach = on_attach,
       view = {
         width = 30,
-        mappings = {
-          custom_only = true, -- disable the default keymaps
-          list = {
-            { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-            { key = "<C-e>",                          action = "edit_in_place" },
-            { key = "O",                              action = "edit_no_picker" },
-            { key = { "<C-]>", "<2-RightMouse>" },    action = "cd" },
-            { key = "<C-v>",                          action = "vsplit" },
-            { key = "<C-x>",                          action = "split" },
-            { key = "<C-t>",                          action = "tabnew" },
-            { key = "<",                              action = "prev_sibling" },
-            { key = ">",                              action = "next_sibling" },
-            { key = "P",                              action = "parent_node" },
-            { key = "<BS>",                           action = "close_node" },
-            { key = "<Tab>",                          action = "preview" },
-            { key = "E",                              action = "first_sibling" },
-            { key = "N",                              action = "last_sibling" },
-            { key = "C",                              action = "toggle_git_clean" },
-            { key = "H",                              action = "toggle_dotfiles" },
-            { key = "B",                              action = "toggle_no_buffer" },
-            { key = "U",                              action = "toggle_custom" },
-            { key = "R",                              action = "refresh" },
-            { key = "a",                              action = "create" },
-            { key = "d",                              action = "remove" },
-            { key = "D",                              action = "trash" },
-            { key = "r",                              action = "rename" },
-            { key = "<C-r>",                          action = "full_rename" },
-            { key = "x",                              action = "cut" },
-            { key = "c",                              action = "copy" },
-            { key = "p",                              action = "paste" },
-            { key = "y",                              action = "copy_name" },
-            { key = "Y",                              action = "copy_path" },
-            { key = "gy",                             action = "copy_absolute_path" },
-            { key = "[e",                             action = "prev_diag_item" },
-            { key = "[c",                             action = "prev_git_item" },
-            { key = "]e",                             action = "next_diag_item" },
-            { key = "]c",                             action = "next_git_item" },
-            { key = "u",                              action = "dir_up" },
-            { key = "s",                              action = "system_open" },
-            { key = "f",                              action = "live_filter" },
-            { key = "F",                              action = "clear_live_filter" },
-            { key = "q",                              action = "close" },
-            { key = "W",                              action = "collapse_all" },
-            { key = "E",                              action = "expand_all" },
-            { key = "S",                              action = "search_node" },
-            { key = ".",                              action = "run_file_command" },
-            { key = "<C-k>",                          action = "toggle_file_info" },
-            { key = "g?",                             action = "toggle_help" },
-            { key = "m",                              action = "toggle_mark" },
-            { key = "bmv",                            action = "bulk_move" },
-          },
-        },
-      },
+      }
     })
   end
 }

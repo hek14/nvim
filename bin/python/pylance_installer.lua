13fbc54adf97ea05808de4505b30d9144f980d0a
hek14 <1023129548@qq.com>
2023-08-29 16:59:33 +0800

    fix pylance_installer scirpt

--- bin/python/pylance_installer.lua
+++ bin/python/pylance_installer.lua
@@ -4,13 +4,14 @@ local sed_path = is_mac and '/Users/hk/.local/bin/gsed' or 'sed'
 local uv = vim.loop
 local Job = require'plenary.job'
 local script0 = [[mkdir tmp]]
-local script1 = [[curl -s -c cookies.txt https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance]]
-local script2 = [[curl -s https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/latest/vspackage -j -b cookies.txt --compressed --output pylance.vsix]]
-local script3 = [[unzip pylance.vsix]]
-local script4 = {sed_path, '-i', [[0,/\(if(\!process\[[^] ]*\]\[[^] ]*\])return\!0x\)1/ s//\10/]], 'extension/dist/server.bundle.js'}
-local script5 = {'mv', vim.fn.expand("~/.config/nvim/tmp"), vim.fn.expand("~/.config/nvim/bin/python/pylance_latest")}
+local script1 = [[rm -rf ~/.config/nvim/bin/python/pylance_latest]]
+local script2 = [[curl -s -c cookies.txt https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance]]
+local script3 = [[curl -s https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/latest/vspackage -j -b cookies.txt --compressed --output pylance.vsix]]
+local script4 = [[unzip pylance.vsix]]
+local script5 = {sed_path, '-i', [[0,/\(if(\!process\[[^] ]*\]\[[^] ]*\])return\!0x\)1/ s//\10/]], 'extension/dist/server.bundle.js'}
+local script6 = [[mv ~/.config/nvim/tmp ~/.config/nvim/bin/python/pylance_latest]]
 
-local todo = {script0, script1, script2, script3, script4, script5}
+local todo = {script0, script1, script2, script3, script4, script5, script6}
 local function go(index)
   if(index>#todo) then
     vim.notify("all job done", vim.log.levels.INFO)
@@ -20,6 +21,9 @@ local function go(index)
   local comps = todo[index]
   if type(todo[index])=="string" then
     comps = require('core.utils').stringSplit(todo[index], ' ')
+  end
+  for i,c in ipairs(comps) do
+    comps[i] = string.gsub(c,"~",vim.fn.expand("~"))
   end
   vim.print(comps)
   Job:new({

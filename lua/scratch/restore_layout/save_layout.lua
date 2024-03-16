6f6422948a00bdcc26f426cfdeb1896e1798c48f
hek14 <hek14thu@gmail.com>
2024-03-17 00:54:41 +0800

    [fix]: should fix now, the core solution is to avoid using nvim_win_set_config, use nvim_win_set_width instead!!!

--- lua/scratch/restore_layout/save_layout.lua
+++ lua/scratch/restore_layout/save_layout.lua
@@ -3,7 +3,7 @@ function M.get_layout()
     local current_winnr = nil;
     local current_winId = vim.api.nvim_get_current_win()
     local windows = vim.api.nvim_tabpage_list_wins(0)
-    local layoutInfo = {}
+    local wins = {}
     for _, winId in ipairs(windows) do
         local pos = vim.api.nvim_win_get_position(winId)
         local winnr = vim.api.nvim_win_get_number(winId)
@@ -15,11 +15,30 @@ function M.get_layout()
         local cursor = vim.api.nvim_win_get_cursor(winId)
         local config = vim.api.nvim_win_get_config(winId)
         config = vim.tbl_deep_extend("force", config, {winnr = winnr, x = pos[2], y = pos[1], isleaf = false, file = file, cursor = cursor})
-        if(config.focusable and config.relative == "") then -- NOTE:ignore the floating windows and buffers that don't attach to files
-          table.insert(layoutInfo, config)
+        table.insert(wins, config)
+    end
+    return {
+        wins = wins,
+        current_winnr = current_winnr
+    }
+end
+
+function M.get_split_layout()
+    local windows = vim.api.nvim_tabpage_list_wins(0)
+    local wins = {}
+    for _, winId in ipairs(windows) do
+        local pos = vim.api.nvim_win_get_position(winId)
+        local winnr = vim.api.nvim_win_get_number(winId)
+        local bufnr = vim.api.nvim_win_get_buf(winId)
+        local file = vim.api.nvim_buf_get_name(bufnr)
+        local cursor = vim.api.nvim_win_get_cursor(winId)
+        local config = vim.api.nvim_win_get_config(winId)
+        config = vim.tbl_deep_extend("force", config, {winnr = winnr, x = pos[2], y = pos[1], isleaf = false, file = file})
+        if(config.focusable and config.relative == "") then
+            table.insert(wins, config)
         end
     end
-    layoutInfo.current_winnr =  current_winnr
-    return layoutInfo
+    return wins
 end
+
 return M

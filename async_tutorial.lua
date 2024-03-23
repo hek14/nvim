5c1a9c0865cf104c613b6fd81bcdc14a9964018b
hek14 <hek14thu@gmail.com>
2024-03-23 23:39:10 +0800

    [NOTE] important! about how to use `lua-async-await` to sync vim.lsp.buf_request!!!

--- async_tutorial.lua
+++ async_tutorial.lua
@@ -58,7 +58,7 @@ end
 -- typical nodejs / luv function
 local echo_2 = function (msg1, msg2, callback)
   -- wait 1000ms
-  timeout(1000, function ()
+  timeout(500, function ()
     -- NOTE: 代表async op已经处理完得到结果, 这里`callback(msg1, msg2)`相当于`return msg1, msg2;`
     -- 可以任意加工返回任何值: 比如 `callback(2*msg1, 3*msg2)`
     callback(msg1, msg2)
@@ -75,7 +75,31 @@ end
 -- 使用 `a.wait(e2(input_1, input_2))` 会等待echo_2这个异步op结束, 同步化!
 -- echo_2(异步) -> e2(同步)
 -- thunkify echo_2
-local e2 = a.wrap(echo_2)
+local e2 = a.wrap(echo_2) -- NOTE: e2 调用时的参数是echo_2的前两个参数: msg1和msg2, callback不用给, 它是a.wait()自动赋值的, 相当于return
+
+
+-- NOTE:
+-- 这里给出一个更加实际的例子:
+-- lsp_request是一个经典的async op, 我们希望写一个函数, 在函数内发出lsp request, 然后等待结果出来之后再处理它(意味着发出request和process result必须是同步的)
+-- 但是我们又不想block nvim
+-- 这个需求意味着我们必须让这个函数变成awaitable, 或者说可以打断的coroutine, 这样不会block整个nvim线程, 而是只暂停自己
+-- 方法是利用a.wrap定义出来这样一个基础的sync op出来
+-- 值得注意的是: a.wrap(fn) fn这个function的signature中, 输入参数的最后一个一定是callback函数: handle result的函数, 这是由a.wrap中pong函数所决定的
+local lsp_async_request = function(callback) -- 可以输入其他参数, 但是callback一定是最后一个
+  local buf = vim.api.nvim_get_current_buf()
+  local params = vim.lsp.util.make_position_params()
+  params.context = {}
+  vim.lsp.buf_request(buf, "textDocument/references", params, function(err, result, ctx, config)
+    if(err or result == nil or vim.tbl_isempty(result)) then
+      callback(nil) -- 相当于yield nil
+    else
+      callback(result) -- 相当于yield result
+    end
+  end)
+end
+
+
+local lsp_sync_but_not_blocking = a.wrap(lsp_async_request) -- NOTE: 变成awaitable
 
 
 local async_tasks_1 = function()
@@ -102,15 +126,26 @@ local async_tasks_2 = function (val)
   end)
 end
 
+local main_loop = function(f)
+  vim.schedule(f)
+end
+
 
 local async_example = function ()
   return a.sync(function ()
     -- composable, await other async thunks
     -- NOTE: 这里没使用wait_all, 所以async_tasks_1和async_tasks_2是串行的或者说依次执行的
-    -- a.wait(xxx) xxx必须是 a.sync()或者a.wrap() 所wrap的一个function
+    -- a.wait(xxx) xxx可以是其他async def(也就是说async def可以组合), 也可以是a.wrap(), 但是async def最终必须是a.wrap()的东西, 最本质的sync op还是a.wrap(), 类似于python中最本质的awaitable还是`asyncio.sleep()`
     local u = a.wait(async_tasks_1())
     local v = a.wait(async_tasks_2(3)) -- NOTE: awaitable返回值可以是任何东西, 这和一般的function一样
+    a.wait(main_loop)
+    local my = a.wait(lsp_sync_but_not_blocking())
     print(u + v())
+    if my then
+      vim.print({my = my})
+    else
+      print("my is nil")
+    end
   end)
 end
 
@@ -152,6 +187,11 @@ end
 -- NOTE: 最终root节点怎么调用呢: python中是`asyncio.run(root_coroutine)`, 这里是直接root_coroutine()
 -- textlock_succ()()
 
+-- NOTE: 一定要注意: await一定只能用在a.sync wrap的function中, 不能直接用于普通的function中, 更不能直接在文件中写`a.wait(async_example())`, 这和python `await`一样.
+local test = a.sync(function()
+  a.wait(async_example())
+end)
+test()
 
 return {
   sync_example = sync_example,
